ada9bb38299768b9f2a91ecbf5e53d38b5deb664
azimisaki <azimisaki@outlook.com>
2024-03-14 18:48:17 +0800

    refactor:  Refactor webpack configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: false,
-  basePath: '/css-to-tailwind',
-  assetPrefix: `/css-to-tailwind/`,
-  webpack(config) {
+  // basePath: '/css-to-tailwind',
+  // assetPrefix: `/css-to-tailwind/`,
+  webpack (config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [{ loader: '@svgr/webpack', options: { icon: true } }]
-    })
+    });
 
-    return config
+    return config;
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
