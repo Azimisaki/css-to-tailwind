f97ed58b6b855fc9868fcb864a2e72199e396338
azimisaki <azimisaki@users.noreply.github.com>
2024-03-14 10:39:15 +0000

    deploy: 6e5c8cc953ad2a50f3ebc95acd8b4321863c762c

---
+++ _next/static/KJT9xaFqaaXyiNQYLkYJj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/976-be8fd0e4a1b53723.js","static/chunks/pages/index-a90ba337a5c742c4.js"],"/_error":["static/chunks/pages/_error-f7977824d9b305ed.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
